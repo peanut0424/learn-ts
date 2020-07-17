b32664c1d643453d06011d690a955f66a4d12006
Peanut <Peanut04221996@gmail.com>
2020-07-17 20:03:44 +0800

    20200717

--- 05.循环语句/02.for练习.ts
+++ 05.循环语句/02.for练习.ts
@@ -3,3 +3,18 @@
 for (let i = 1; i <= 5; i++) {
     console.log(i)
 }
+
+// for 判断素数
+for (let i = 2; i <= 100; i++) {
+    let flag = true
+    for (let j = 2; j < i; j++) {
+        if (i % j === 0) {
+            flag = false
+            break
+        }
+    }
+
+    if (flag) {
+        console.log(i + '是素数')
+    }
+}
