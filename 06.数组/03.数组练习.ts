8195f64ded3381f13fa3f7b22418fdd536afd150
Peanut <Peanut04221996@gmail.com>
2020-07-18 16:46:46 +0800

    20200718

--- 06.数组/03.数组练习.ts
+++ 06.数组/03.数组练习.ts
@@ -1,11 +1,23 @@
 // 练习1:求数组中所有元素的和
 let sum: number = 0
 
-let arr: number[] = [200, 30, 3]
+let arr1: number[] = [200, 30, 3]
 
-for (let i: number = 0; i < arr.length; i++) {
-    sum += arr[i]
+for (let i: number = 0; i < arr1.length; i++) {
+    sum += arr1[i]
 }
 
 console.log('sum = ' + sum);
 
+// 找出数组中的最大值
+
+let arr2: number[] = [1, 9, 3]
+let max: number = arr2[0]
+
+for (let i: number = 1; i < arr2.length; i++) {
+    if (arr2[i] > max) {
+        max = arr2[i]
+    }
+}
+
+console.log('max = ' + max)
